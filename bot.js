d8741da7fe66585fb09156e92baf29ee9e136080
HanSaloZu <conswintcomin@gmail.com>
2020-12-30 00:47:36 +0300

    Create message processing
    Added parsing of commands and their arguments

--- bot.js
+++ bot.js
@@ -17,4 +17,20 @@ client.on("disconnect", () => {
   console.log("Disconnect");
 });
 
+client.on("message", async (message) => {
+  const prefix = config.get("prefix");
+  if (message.author.bot || !message.content.startsWith(prefix)) return;
+
+  const args = message.content.trim().slice(prefix.length).split(/ +/);
+  const commandName = args.shift();
+  const command = commands.get(commandName);
+
+  try {
+    command.execute(message, args, client);
+  } catch (error) {
+    console.log(error);
+    message.channel.send("Error...Something went wrong");
+  }
+});
+
 client.login(config.get("token"));
