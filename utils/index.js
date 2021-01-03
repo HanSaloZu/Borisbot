ca6aa0e4ee3cacdcbff22d43e188ca7eac254596
HanSaloZu <conswintcomin@gmail.com>
2021-01-03 23:26:14 +0300

    Add a function to generate a string of mentions from the list of users

--- utils/index.js
+++ utils/index.js
@@ -1,11 +1,13 @@
 const { createCommonMessage, createErrorMessage } = require("./messages");
 const getUserIdFromMention = require("./getUserIdFromMention");
+const generateMentionsString = require("./generateMentionsString");
 const { MentionRequiredError, PermissionError } = require("./errors");
 
 module.exports = {
   createCommonMessage,
   createErrorMessage,
   getUserIdFromMention,
+  generateMentionsString,
   MentionRequiredError,
   PermissionError
 };
