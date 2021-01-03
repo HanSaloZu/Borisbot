2cb71f3c16bec2c3597874e088739a21b21ee0f0
HanSaloZu <conswintcomin@gmail.com>
2021-01-03 21:33:12 +0300

    Replace the user's nickname with a mention in the reply message

--- commands/members/joinedAt.js
+++ commands/members/joinedAt.js
@@ -4,18 +4,14 @@ const dateFormat = require("../../config").get("dateFormat");
 module.exports = {
   name: "joined-at",
   description: "Sends the time the user joined the guild",
-  execute(message, args, client) {
-    const userId = getUserIdFromMention(args[0]);
-    const username = client.users.cache.get(userId).username;
-    const guildMember = message.guild.members.cache.get(userId);
+  execute(message, args) {
+    const guildMember = message.guild.members.cache.get(
+      getUserIdFromMention(args[0])
+    );
 
     message.channel.send(
       createCommonMessage().setDescription(
-        `${
-          guildMember.nickname
-            ? `${guildMember.nickname}(${username})`
-            : username
-        } was joined this guild at ${guildMember.joinedAt.toLocaleDateString(
+        `${guildMember.user.toString()} was joined this guild at ${guildMember.joinedAt.toLocaleDateString(
           "en-US",
           dateFormat
         )}`
--- commands/members/registeredAt.js
+++ commands/members/registeredAt.js
@@ -5,15 +5,11 @@ module.exports = {
   name: "registered-at",
   description: "Sends the time the user was registered at",
   execute(message, args, client) {
-    const userId = getUserIdFromMention(args[0]);
-    const user = client.users.cache.get(userId);
-    const userNickname = message.guild.members.cache.get(userId).nickname;
+    const user = client.users.cache.get(getUserIdFromMention(args[0]));
 
     message.channel.send(
       createCommonMessage().setDescription(
-        `${
-          userNickname ? `${userNickname}(${user.username})` : user.username
-        } was registered at ${user.createdAt.toLocaleDateString(
+        `${user.toString()} was registered at ${user.createdAt.toLocaleDateString(
           "en-US",
           dateFormat
         )}`
