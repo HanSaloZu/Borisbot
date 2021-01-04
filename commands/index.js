fca10dd27284242f42522cd1b14b260404619a9a
HanSaloZu <conswintcomin@gmail.com>
2021-01-04 16:05:20 +0300

    Change how commands are added to the main command object

--- commands/index.js
+++ commands/index.js
@@ -1,15 +1,12 @@
-const { guildOwner, memberCount, createdAt } = require("./guild");
-const { registeredAt, joinedAt, ban, kick } = require("./members");
-
+const commandsObject = Object.assign(
+  {},
+  require("./guild"),
+  require("./members")
+);
 const commands = new Map();
 
-commands.set(memberCount.name, memberCount);
-commands.set(guildOwner.name, guildOwner);
-commands.set(createdAt.name, createdAt);
-
-commands.set(joinedAt.name, joinedAt);
-commands.set(registeredAt.name, registeredAt);
-commands.set(ban.name, ban);
-commands.set(kick.name, kick);
+for (const [, command] of Object.entries(commandsObject)) {
+  commands.set(command.name, command);
+}
 
 module.exports = commands;
