0c04997da1d84354f5033af3f4a53d3a64a648e3
HanSaloZu <conswintcomin@gmail.com>
2021-01-12 16:08:11 +0300

    Fix. Remove useless code

--- utils/getUserIdFromMention.js
+++ utils/getUserIdFromMention.js
@@ -1,8 +1,6 @@
-const { MentionRequiredError, InvalidMentionError } = require("./errors");
+const { InvalidMentionError } = require("./errors");
 
 module.exports = (mention) => {
-  if (!mention) throw new MentionRequiredError();
-
   if (mention.startsWith("<@") && mention.endsWith(">")) {
     mention = mention.slice(2, -1);
     if (mention.startsWith("!")) mention = mention.slice(1);
