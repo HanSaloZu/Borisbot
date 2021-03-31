45da106b992433469670506e164d701bd3009c98
HanSaloZu <conswintcomin@gmail.com>
2021-04-01 01:37:09 +0300

    Prettier help command code

--- commands/help.js
+++ commands/help.js
@@ -12,11 +12,21 @@ module.exports = {
         .setTitle("About bot")
         .setURL("https://github.com/HanSaloZu/Borisbot/tree/master")
         .setDescription(
-          "This bot will help you simplify the process of moderating your discord server. Bot can ban/unban/kick server members, delete messages and more."
+          "This bot will help you simplify the process of moderating your discord server. Bot can ban/kick server members, delete messages and more."
         )
         .addField(
           "Commands",
-          `Category: guild \n \`${pref}guild-info\` \n\n Category: guild members \n \`${pref}user-info @<username>\` \n \`${pref}ban @<username> @<username> ...\` \n \`${pref}kick @<username> @<username> ...\` \n\n Category: messages \n \`${pref}purge <amount> @<username>\` \n\n Category: other \n \`${pref}version\` \n\n To get detailed information about the command, type \`${pref}help <command>\``
+          "Category: guild \n" +
+            `\`${pref}guild-info\` \n\n` +
+            "Category: guild members \n" +
+            `\`${pref}user-info @<username>\` \n` +
+            `\`${pref}ban @<username> @<username> ...\` \n` +
+            `\`${pref}kick @<username> @<username> ...\` \n\n` +
+            "Category: messages \n" +
+            `\`${pref}purge <amount> @<username>\` \n\n` +
+            "Category: other \n" +
+            `\`${pref}version\` \n\n` +
+            `To get detailed information about the command, type \`${pref}help <command>\``
         );
     } else {
       const command = commands.get(args[0]);
